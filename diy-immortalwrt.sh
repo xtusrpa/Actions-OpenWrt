6abd131c233a7f7eaa82c514911428686f9e5f92
xtusrpa <56962848+xtusrpa@users.noreply.github.com>
2026-05-17 06:03:12 +0800

    Update diy-immortalwrt.sh

--- diy-immortalwrt.sh
+++ diy-immortalwrt.sh
@@ -19,11 +19,16 @@ sed -i 's/192.168.1.1/192.168.88.166/g' package/base-files/files/bin/config_gene
 # Modify hostname
 sed -i 's/ImmortalWrt/Immortal-Router/g' package/base-files/files/bin/config_generate
 
-# 1. 创建 OpenClash 内核的存放目录
+# 1. 创建 OpenClash 内核存放目录
 mkdir -p files/etc/openclash/core
 
-# 2. 下载 Meta 内核 (x86_64 架构)
+# 2. 下载 Meta 内核，并解压重命名为 clash_meta
+echo "Downloading Meta Core..."
 wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar xOvz > files/etc/openclash/core/clash_meta
 
-# 3. 赋予内核执行权限
+# 3. 下载 Premium (TUN) 内核，并解压重命名为 clash_tun
+echo "Downloading TUN Core..."
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17.tar.gz | tar xOvz > files/etc/openclash/core/clash_tun
+
+# 4. 赋予所有内核最高执行权限
 chmod +x files/etc/openclash/core/clash*
