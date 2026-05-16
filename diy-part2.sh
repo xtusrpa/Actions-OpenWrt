6377ef9892528312ac40c8e8ed53ed8c811884ff
xtusrpa <56962848+xtusrpa@users.noreply.github.com>
2026-05-16 18:42:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,5 @@ sed -i 's/192.168.1.1/192.168.88.166/g' package/base-files/files/bin/config_gene
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/MyRouter/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/MyRouter/g' package/base-files/files/bin/config_generate
